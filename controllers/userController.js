614b63d5a7c16948cc19352f2c0ec19505edfa01
Kardi <kgland06@gmail.com>
2024-09-28 09:17:07 +0700

    register user

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+
+//@desc Register User
+//@route Post /api/user/register
+//@access public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("all fields are required");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+    if(user){
+        res.status(201).json({_id: user.id, email: user.email});
+    }else{
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+    res.json({message: "Register User"})
+});
+
+//@desc Login User
+//@route Post /api/user/login
+//@access public
+
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message: "Login User"})
+});
+
+
+//@desc Current User
+//@route Post /api/user/current
+//@access private
+
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({message: "Current User"})
+});
+
+module.exports = {
+    registerUser,
+    loginUser,
+    currentUser
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contactRoute = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/contacts', contactRoute);
+app.use('/api/user', userRoutes);
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
